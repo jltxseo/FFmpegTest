9bf525607e934ba1a5bd2f3a8d161e7a321f133e
jltxseo <2529231020@qq.com>
2018-07-20 01:30:33 +0800

    使用cmake编辑ffmpeg

--- app/jni/Application.mk
+++ app/jni/Application.mk
@@ -9,7 +9,7 @@ APP_STL := stlport_static
 #APP_STL := gnustl_shared  #shared_ptr用到
 APP_CPPFLAGS := -fexceptions # -D SOUNDTOUCH_DISABLE_X86_OPTIMIZATIONS
 
-NDK_TOOLCHAIN_VERSION := 4.8
+NDK_TOOLCHAIN_VERSION := 4.9
 APP_CPPFLAGS += -std=c++0x
 APP_CPPFLAGS += -fexceptions
 APP_CPPFLAGS += -frtti
